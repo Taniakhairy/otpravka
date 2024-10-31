0db0240bb37226e2e7de3d990c0ffbafc397436c
Taniakhairy <t0026585@gmail.com>
2024-10-31 21:13:29 +0300

    Добавлена обработка исключений

--- otpravka.py
+++ otpravka.py
@@ -8,10 +8,13 @@ text = 'Hello word!'
 
 url=f"http://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
 print(url)
-response = requests.get(url)
-print(response)
+try:
+    response = requests.get(url)
+    print(response)
 
-if response.status_code == 200:
-    print("Сообщение успешно отправлено!")
-else:
-    print(f"Ошибка при отправке{response.status_code}")
+    if response.status_code == 200:
+        print("Сообщение успешно отправлено!")
+    else:
+        print(f"Ошибка при отправке{response.status_code}")
+except Exception as e:
+    print(f"Непредвиденная ошибка с кодом{e}")
